8c74815bfd044007238bce64ce1abacc844f7593
mary-foxen <118937567+mary-foxen@users.noreply.github.com>
2022-11-30 01:01:38 -0500

    Add files via upload
    updated automate_comsol. this one should return data frame with the k effective values column appended.

--- automate_comsol.py
+++ automate_comsol.py
@@ -1,20 +1,26 @@
 import mph
+import pandas as pd
+
 client = mph.start(cores=1)
 model = client.load('graphite-thermal-conductivity.mph')
 importNode = model/'geometries'/'Geometry 1'/'Import 1'
 
-#list for the different spline points used in sketches
-pts = ['10','20','30','40']
 values = []
 
-for i in pts:
-    for x in range(5):
-        importNode.property('filename','graphite_automated_split v1' + i +' pts' + str(x) +'.stp')
-        model.build()
-        model.mesh()
-        model.solve()
-        values.append(model.evaluate('bnd1'))
 
-        print(values)
+df = pd.read_csv("data.csv")
+files = df.loc[:,"filename"]
+for f in files:
+    importNode.property('filename', str(f))
+    model.build()
+    model.mesh()
+    model.solve()
+    values.append(model.evaluate('bnd1'))
 
-print(values)
+
+# Using 'eff_k' as the column name
+# and equating it to the list values
+df['eff_k'] = values
+
+print(df)
+
