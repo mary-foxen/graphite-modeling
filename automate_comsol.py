66311f119df46dbcce753bf13ed796d28d68f5aa
shomikverma <shomik.verma@gmail.com>
2022-12-02 23:08:47 -0500

    added 2D version of comsol file

--- automate_comsol.py
+++ automate_comsol.py
@@ -1,29 +1,39 @@
 import mph
 import pandas as pd
+import numpy as np
 
-client = mph.start(cores=1)
-model = client.load('graphite-thermal-conductivity.mph')
+
+# mph.option('stand-alone')
+print('starting client')
+client = mph.start(cores=4)
+print('loading model')
+model = client.load('graphite-thermal-conductivity-2D.mph')
 importNode = model/'geometries'/'Geometry 1'/'Import 1'
 
 values = []
 
-
-df = pd.read_csv("data.csv")
-files = df.loc[:,"filename"]
+print('reading csv')
+df = pd.read_csv("fusion add-in/data.csv")
+files = df.loc[:, "filename"]
 for f in files:
+    print('setting filename')
     importNode.property('filename', str(f))
+    print('building model')
     model.build()
+    print('meshing model')
     model.mesh()
+    print('solving model')
     model.solve()
-    values.append(model.evaluate('bnd1'))
+    print('evaluating effective k')
+    values.append(np.atleast_1d(model.evaluate('bnd1'))[0])
+    print(values)
 
 
 # Using 'eff_k' as the column name
 # and equating it to the list values
 df['eff_k'] = values
 
-#save back to data.csv file
-df.to_csv("data.csv", index=False)
+# save back to data.csv file
+df.to_csv("comsol_data.csv", index=False)
 
 print(df)
-
--- fusion add-in/test1.py
+++ fusion add-in/test1.py
@@ -1,43 +1,47 @@
-#Author-
-#Description-
+# Author-
+# Description-
 
-import os, sys
+import os
+import sys
 import random
-import adsk.core, adsk.fusion, adsk.cam, traceback, math 
+import adsk.core
+import adsk.fusion
+import adsk.cam
+import traceback
+import math
 
 
 def run(context):
     ui = None
     try:
-        
+
         app = adsk.core.Application.get()
-        ui  = app.userInterface
+        ui = app.userInterface
         ui.messageBox('Hello script')
-        
-        
-        # get active design        
+
+        # get active design
         product = app.activeProduct
         design = adsk.fusion.Design.cast(product)
-         
+
         # get all components in this design
         allComps = design.allComponents
-         
+
         # get the script location
-        scriptDir = os.path.dirname(os.path.realpath(__file__))        
-         
+        scriptDir = os.path.dirname(os.path.realpath(__file__))
+
         # create a single exportManager instance
         exportMgr = design.exportManager
 
-        num_spline_points = [10,20,30,40]
+        num_spline_points = [10, 20, 30, 40]
 
         textfile = scriptDir + "/" + 'data' + '.csv'
 
-        with open(textfile,'w') as f:
+        with open(textfile, 'w') as f:
             f.write('filename,num_point,pore_depth,area_top,area_bottom\n')
-        
-        for num in num_spline_points: #traces through num_spline_points to generate different porosities
 
-            for x in range(5): #creates a certain number of designs
+        for num in num_spline_points:  # traces through num_spline_points to generate different porosities
+
+            for x in range(5):  # creates a certain number of designs
 
                 des = app.activeProduct
                 # Get the root component of the active design.
@@ -47,72 +51,69 @@ def run(context):
                 xyPlane = rootComp.xYConstructionPlane
                 sketch = sketches.add(xyPlane)
 
-                #create left line boundary of graphite
+                # create left line boundary of graphite
                 left_side = adsk.core.ObjectCollection.create()
-                left_side.add(adsk.core.Point3D.create(0,0,0))
-                left_side.add(adsk.core.Point3D.create(0,.2,0))
+                left_side.add(adsk.core.Point3D.create(0, 0, 0))
+                left_side.add(adsk.core.Point3D.create(0, .2, 0))
                 sketch.sketchCurves.sketchFittedSplines.add(left_side)
 
-                #create right line boundary of graphite
+                # create right line boundary of graphite
                 right_side = adsk.core.ObjectCollection.create()
-                right_side.add(adsk.core.Point3D.create(1,0,0))
-                right_side.add(adsk.core.Point3D.create(1,.2,0))
+                right_side.add(adsk.core.Point3D.create(1, 0, 0))
+                right_side.add(adsk.core.Point3D.create(1, .2, 0))
                 sketch.sketchCurves.sketchFittedSplines.add(right_side)
 
-                #create middle line
+                # create middle line
                 mid_line = adsk.core.ObjectCollection.create()
-                mid_line.add(adsk.core.Point3D.create(0,.1,0))
-                mid_line.add(adsk.core.Point3D.create(1,.1,0))
+                mid_line.add(adsk.core.Point3D.create(0, .1, 0))
+                mid_line.add(adsk.core.Point3D.create(1, .1, 0))
                 sketch.sketchCurves.sketchFittedSplines.add(mid_line)
 
-                points_bottom = adsk.core.ObjectCollection.create() # Create an object collection for the points.
+                points_bottom = adsk.core.ObjectCollection.create()  # Create an object collection for the points.
                 points_top = adsk.core.ObjectCollection.create()
-                
+
                 # Enter variables here. E.g. E = 50
-                startRange = 0 # Start of range to be evaluated.
-                endRange = 1 # End of range to be evaluated.
-                splinePoints = num # Number of points that splines are generated.
+                startRange = 0  # Start of range to be evaluated.
+                endRange = 1  # End of range to be evaluated.
+                splinePoints = num  # Number of points that splines are generated.
                 # WARMING: Using more than a few hundred points may cause your system to hang.
 
-                #make the spline start at origin
-                points_bottom.add(adsk.core.Point3D.create(0,0,0))
+                # make the spline start at origin
+                points_bottom.add(adsk.core.Point3D.create(0, 0, 0))
 
-                points_top.add(adsk.core.Point3D.create(0,.2,0))
-                
+                points_top.add(adsk.core.Point3D.create(0, .2, 0))
+
                 #area = areaProps.area
-
 
                 i = 1
                 while i <= (splinePoints-1):
                     t = startRange + ((endRange - startRange)/splinePoints)*i
                     xCoord = t
-                    yCoord_bottom = random.uniform(0,0.04)
-                    yCoord_top = random.uniform(.16,.2)
+                    yCoord_bottom = random.uniform(0, 0.04)
+                    yCoord_top = random.uniform(.16, .2)
                     zCoord = 0
-                        
-                    points_bottom.add(adsk.core.Point3D.create(xCoord,yCoord_bottom,zCoord))
-                    points_top.add(adsk.core.Point3D.create(xCoord,yCoord_top,zCoord))
+
+                    points_bottom.add(adsk.core.Point3D.create(xCoord, yCoord_bottom, zCoord))
+                    points_top.add(adsk.core.Point3D.create(xCoord, yCoord_top, zCoord))
                     i = i + 1
 
-                #make the splines come back to end point
-                points_bottom.add(adsk.core.Point3D.create(1,0,0))
-                points_top.add(adsk.core.Point3D.create(1,.2,0))
+                # make the splines come back to end point
+                points_bottom.add(adsk.core.Point3D.create(1, 0, 0))
+                points_top.add(adsk.core.Point3D.create(1, .2, 0))
 
-                #Generates the spline curve
+                # Generates the spline curve
                 sketch.sketchCurves.sketchFittedSplines.add(points_bottom)
                 sketch.sketchCurves.sketchFittedSplines.add(points_top)
-                
 
                 # Get the profile defined by the circle
                 prof = sketch.profiles.item(0)
                 prof2 = sketch.profiles.item(1)
 
-
                 # Get area properties from a profile
                 areaProps = prof.areaProperties(adsk.fusion.CalculationAccuracy.MediumCalculationAccuracy)
                 areaProps2 = prof2.areaProperties(adsk.fusion.CalculationAccuracy.MediumCalculationAccuracy)
 
-                #get area 
+                # get area
                 area = areaProps.area
                 area2 = areaProps2.area
 
@@ -129,28 +130,26 @@ def run(context):
                 # Create the extrusion
                 extrudeFeature = extrudes.add(extInput)
                 extrudeFeature2 = extrudes.add(extInput2)
-                
 
-                
                 # export the component one by one with a specified format
                 for comp in allComps:
-                    compName = comp.name 
+                    compName = comp.name
                     fileName = scriptDir + "/" + compName + str(num) + ' pts' + str(x)
 
                     # export the component with STP format
-                    stpOptions = exportMgr.createSTEPExportOptions(fileName, comp)
-                    exportMgr.execute(stpOptions)
+                    # stpOptions = exportMgr.createSTEPExportOptions(fileName, comp)
+                    # exportMgr.execute(stpOptions)
+                sketch.saveAsDXF(fileName + '.dxf')
                 area_top = 0.2 - area
-                area_bottom = 0.2 - area2 #areas are currently in cm^2
+                area_bottom = 0.2 - area2  # areas are currently in cm^2
 
-                with open(textfile,'a') as f:
-                    f.write(str(fileName) + '.stp' + ',' + str(num) + ',' + str(0.4) + ',' + str(area_top) + ','+ str(area_bottom)+'\n')
-                
+                with open(textfile, 'a') as f:
+                    f.write(str(fileName) + '.dxf' + ',' + str(num) + ',' + str(0.4) +
+                            ',' + str(area_top) + ',' + str(area_bottom)+'\n')
+
                 extrudeFeature.deleteMe()
                 extrudeFeature2.deleteMe()
                 sketch.deleteMe()
-
-                
 
     except:
         if ui:
